7afd59c67d4b62d148bd0021157f13a909137b6f
Michael Beaton <beatonma@gmail.com>
2018-02-01 19:31:17 +0000

    All http verbs now include a 1-second sleep period to comply with Toggl API rate guidelines. create_time_entry() now accepts a raw timestamp and handles the formatting itself. Implemented lookup of default workspace id based on a name given in config file. Added ApiError class for handling any http errors received

--- toggl_api.py
+++ toggl_api.py
@@ -1,16 +1,39 @@
 import json
+import logging
 import requests
 
 from base64 import b64encode
+from datetime import datetime, timezone
+from time import sleep
 
 
 API_BASE = 'https://www.toggl.com/api/v8/'
 
 
+def _init_logger(name=__file__, level=logging.INFO):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(logging.StreamHandler())
+    return logger
+
+
+logger = _init_logger()
+
+
+class ApiError(Exception):
+    '''
+    Raised whenever an API request receives a response
+    with any 4xx status code.
+    '''
+    pass
+
+
 class TogglApiInterface:
-    def __init__(self, api_key):
+    def __init__(self, config):
+        self.default_workspace = config.get('default_workspace')
+
         self.api_token = b64encode(
-            (api_key + ':api_token').encode()).decode()
+            (config['api_key'] + ':api_token').encode()).decode()
 
         self.headers = {
             'Authorization': 'Basic ' + self.api_token,
@@ -26,16 +49,53 @@ class TogglApiInterface:
             # },
             # ...
         }
+        self.projects = {
+            # 'project_name': {
+            #   'wid': workspace_id,
+            #   'pid': project_id,
+            # },
+            # ...
+        }
 
-        print(self.headers)
+    def __repr__(self):
+        return (
+            'Default workspace: {}\nHeaders: {}\nProjects: {}\nCache: {}'
+            .format(
+                self.default_workspace,
+                self.headers,
+                json.dumps(self.projects, indent=2, sort_keys=True),
+                json.dumps(self.cached, indent=2, sort_keys=True),))
+
+    def _set_default_workspace(self):
+        '''
+        Try to get an integer workspace id.
+        '''
+        if not self.default_workspace:
+            return
+
+        if type(self.default_workspace) is int:
+            return
+
+        if self.cached:
+            for _, w in self.cached.items():
+                if w.get('name', '') == self.default_workspace:
+                    self.default_workspace = w.get('id')
+                    return
 
     def get_workspaces(self):
+        '''
+        Get all workspaces associated with the API token
+        '''
         j = self._get('workspaces')
         self._cache_workspaces(j)
+        self._set_default_workspace()
 
         return j
 
     def get_projects(self, workspace_id):
+        '''
+        Get all projects associated with the workspace
+        '''
         j = self._get(
             'workspaces/{}/projects'.format(workspace_id))
         self._cache_projects(j)
@@ -43,6 +103,9 @@ class TogglApiInterface:
         return j
 
     def get_all_projects(self):
+        '''
+        Get all projects associated with all workspaces for our API token
+        '''
         projects = []
         workspaces = self.get_workspaces()
         if not workspaces:
@@ -51,22 +114,43 @@ class TogglApiInterface:
             projects += self.get_projects(w['id']) or []
         return projects
 
-    def create_project(self, project_name, workspace_id):
+    def create_project(self, project_name, workspace_id=None):
+        '''
+        Create a new project on the given (or default) workspace
+        '''
+        if not workspace_id:
+            workspace_id = self.default_workspace
+            if not workspace_id:
+                raise Exception(
+                    'Cannot create project without a valid workspace ID')
+
         j = self._post('projects', {
             'project': {
                 'wid': workspace_id,
                 'name': project_name,
             }
         })
-        print(j)
+        logger.debug(j)
         self._cache_projects([j['data']])
 
         return j
 
     def delete_project(self, pid):
+        '''
+        Delete the given project. Currently only used for testing.
+        '''
         return self._delete('projects/' + str(pid))
 
-    def create_time_entry(self, project_id, start, duration):
+    def create_time_entry(self, project_id, start_timestamp, duration):
+        if type(project_id) is str:
+            project_id = self.projects[project_id]['pid']
+
+        logger.debug('Creating time entry')
+
+        start = (datetime.fromtimestamp(start_timestamp, timezone.utc)
+                 .astimezone()
+                 .isoformat())
+
         j = self._post('time_entries', {
                 'time_entry': {
                     'pid': project_id,
@@ -75,7 +159,7 @@ class TogglApiInterface:
                     'created_with': 'autotoggl'
                 }
             })
-        print(j)
+        logger.debug(j)
         return j
 
     def _get(self, url_stub):
@@ -84,6 +168,7 @@ class TogglApiInterface:
             headers=self.headers,
         )
         self._show_response(r)
+        sleep(1)
         return r.json()
 
     def _post(self, url_stub, data):
@@ -93,6 +178,7 @@ class TogglApiInterface:
             data=json.dumps(data),
         )
         self._show_response(r)
+        sleep(1)
         return r.json()
 
     def _delete(self, url_stub):
@@ -101,13 +187,14 @@ class TogglApiInterface:
             headers=self.headers,
         )
         self._show_response(r)
+        sleep(1)
+        # return r
         return r.status_code == 200
 
     def _show_response(self, r):
-        print('[{}] {}'.format(r.status_code, r.request.url))
+        logger.info('[{}] {}'.format(r.status_code, r.request.url))
         if r.status_code >= 400:
-            print('Error [{}]: {}'.format(r.status_code, r))
-            print(r.text)
+            raise ApiError('[error:{}] {}'.format(r.status_code, r.text))
 
     def _cache_workspaces(self, workspaces):
         if not workspaces:
@@ -122,6 +209,12 @@ class TogglApiInterface:
         for p in projects:
             wid = str(p['wid'])
             pid = str(p['id'])
+            name = str(p['name'])
             if wid not in self.cached:
                 self.cached[wid] = {}
             self.cached[wid][pid] = p
+
+            self.projects[name] = {
+                'pid': p['id'],
+                'wid': p['wid']
+            }
