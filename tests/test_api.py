ab2a9e021367f84e26faa530feae599f10de2de5
Michael Beaton <beatonma@gmail.com>
2018-02-06 20:02:42 +0000

    Implemented a proper test flow

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,45 +1,48 @@
+from time import sleep
+from datetime import datetime, timezone, timedelta
+
 from autotoggl import toggl_api
 from tests import test_common
+from tests.test_common import equal
+from tests.test_credentials import (
+    TEST_WORKSPACE,
+    TEST_WORKSPACE_ID,
+    TEST_API_KEY,
+)
 
 
-logger = test_common.get_logger(__name__)
+logger = test_common.get_logger(name=__name__)
 
 
 def test_api_interface(config=None):
     config = test_common.get_test_config()
     interface = toggl_api.TogglApiInterface(config)
-    # j = interface.get_workspaces()
-    # logger.info(json.dumps(j, indent=2))
-    # test_api_get_projects(interface)
-    # pid = test_api_create_project(interface)
-    # test_api_create_time_entry(interface, pid)
-    # test_api_delete_project(interface, pid)
-    logger.info(interface)
+
+    interface.get_all_projects()
+
+    project_id = _test_api_create_project(interface)
+    _test_api_create_time_entry(interface, project_id)
+    _test_api_delete_project(interface, project_id)
 
 
-# def test_api_get_projects(interface):
-#     j = interface.get_all_projects()
+def _test_api_create_project(interface):
+    j = interface.create_project('API_TEST_PROJECT', TEST_WORKSPACE_ID)
+    equal(j['data']['wid'], TEST_WORKSPACE_ID)
+
+    pid = j['data']['id']
+    return pid
 
 
-# def test_api_create_project(interface):
-#     j = interface.create_project('API_TEST_PROJECT', TEST_WORKSPACE_ID)
-#     _equal(j['data']['wid'], TEST_WORKSPACE_ID)
-
-#     pid = j['data']['id']
-#     return pid
-
-
-# def test_api_create_time_entry(interface, pid):
-#     j = interface.create_time_entry(
-#         pid,
-#         (datetime.datetime.now(timezone.utc).astimezone() - timedelta(hours=1))
-#         .replace(microsecond=0).isoformat(),
-#         120
-#     )
-#     logger.debug('Time entry response: {}'.format(j))
+def _test_api_create_time_entry(interface, pid):
+    interface.create_time_entry(
+        pid,
+        (datetime.now(timezone.utc).astimezone() - timedelta(hours=1))
+        .replace(microsecond=0).timestamp(),
+        120
+    )
 
 
-# def test_api_delete_project(interface, pid):
-#     sleep(10)
-#     deleted = interface.delete_project(pid)
-#     _equal(deleted, True)
+def _test_api_delete_project(interface, pid):
+    sleep(5)
+    deleted = interface.delete_project(pid)
+    equal(deleted, True)
