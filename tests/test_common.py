3fbe0364da590b38801b28e1f3a666be084ec70e
Michael Beaton <beatonma@gmail.com>
2018-02-06 16:55:31 +0000

    Added comments and some more data to test config

--- tests/test_common.py
+++ tests/test_common.py
@@ -19,7 +19,7 @@ logger = get_logger()
 
 
 def get_test_config():
-    return Config(json_data={
+    return Config(clargs={}, json_data={
         'api_key': TEST_API_KEY,
         'default_workspace': TEST_WORKSPACE,
         'default_day': 'today',
@@ -28,36 +28,68 @@ def get_test_config():
         'project_definitions': [
             {
                 'process': 'sublime_text',
+
+                # If this pattern matches, the first group will be used
+                # as the project title
                 'project_pattern': '.*\\((.*?)\\) - Sublime Text.*',
-                'description_pattern': '(.*?) . \\(.*?\\) - Sublime Text.*'
+
+                # If this pattern matches, the first group will be used
+                # as the event description
+                'description_pattern': ['(.*?) . \\(.*?\\) - Sublime Text.*'],
             },
             {
                 'process': 'studio64',
                 'project_pattern': '(.*?) - \\[.*\\].*',
-                'description_pattern': '.*? - \\[.*?\\] - (.*?) - .*'
+                'description_pattern': ['.*? - \\[.*?\\] - (.*?) - .*'],
             },
             {
                 'process': 'chrome',
                 'projects': [
+                    # List of projects that might match this process
                     {
-                        'title': 'Duolingo',
+                        # If any of the strings in window_contains are
+                        # found in the window title then this will
+                        # be the project title
+                        'project_title': 'Duolingo',
+
+                        # If any of the strings in window_contains are
+                        # found in the window title then this will
+                        # be the event description
                         'description': 'German practice',
+
                         'window_contains': [
                             'duolingo',
-                        ]
+                        ],
                     },
                     {
-                        'title': 'Casual',
+                        'project_title': 'Casual',
                         'description': 'Internetting',
-                        'description_pattern': '(.*)',
+
+                        # If this pattern matches the window title then
+                        # the first group will provide the event
+                        # description. If there is no match, the value
+                        # of `description` will be returned
+                        'description_pattern': [],
                         'window_contains': [
                             'guardian'
-                            'netflix'
                             'news',
                             'politics',
                             'reddit',
                             'starcraft',
-                        ]
+                        ],
+                    },
+
+                    {
+                        'project_title': 'Casual',
+                        'description': '_',
+
+                        'description_pattern': [
+                            'BBC iPlayer - (.*)',
+                        ],
+                        'window_contains': [
+                            'netflix',
+                            'iplayer',
+                        ],
                     },
                 ]
             },
